79f80e9ee90b46f0e67aa3222104739b3b691254
oderathegreat <oderathegreat@gmail.com>
2023-04-27 22:26:04 +0300

    data returns on drinks route

---
+++ BevDrinksApi/views.py
@@ -0,0 +1,14 @@
+from .models import Drinks
+from django.http import JsonResponse
+from .serializers import DrinkSerializer
+
+def showdrinks(request):
+    #get all drinks
+    #serialize them
+    #return data in Json Format
+    all_drinks = Drinks.objects.all()
+    serializer = DrinkSerializer(all_drinks, many=True)
+    return JsonResponse(serializer.data, safe=False)
+
+
+
