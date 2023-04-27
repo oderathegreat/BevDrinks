dad7bb32c7ba1d4260e356954d358c6fc3acf654
oderathegreat <oderathegreat@gmail.com>
2023-04-27 22:03:41 +0300

    rest framework dependancy on settings

--- BevDrinksApi/models.py
+++ BevDrinksApi/models.py
@@ -5,6 +5,5 @@ class Drinks(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
 
-
-def __str__(self):
-    return self.drinkspy
+    def __str__(self):
+        return self.name + '' + self.description
